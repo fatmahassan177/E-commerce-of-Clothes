f4a595faf649809b9f3b79a62d13a36687e3f075
Fatma Hassan <Fatmahassan177@gmail.com>
2025-09-06 21:51:11 +0300

    Update home and layout files, add footer component

--- Front-E-Commerse/src/app/layout/home/home.ts
+++ Front-E-Commerse/src/app/layout/home/home.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { HomeProduct } from "../home-product/home-product";
 import { Testimonials } from '../testimonials/testimonials';
+import { Footer } from "../footer/footer";
+import { FAQ } from "../faq/faq";
+import { Contact } from "../contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, HomeProduct,Testimonials],
+  imports: [Header, HomeProduct, Testimonials, Footer, FAQ, Contact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
--- Front-E-Commerse/src/app/layout/layout.ts
+++ Front-E-Commerse/src/app/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Nav } from './nav/nav';
 import { Header } from './header/header';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-layout',
-  imports: [Nav,RouterOutlet],
+  imports: [Nav, RouterOutlet, Footer],
   templateUrl: './layout.html',
     standalone: true,      
    styleUrls: ['./layout.css'] 
